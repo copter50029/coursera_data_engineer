f3cb399d8da0d1a60961e130b8afbb41394c3ac1
copter50029 <152718101+copter50029@users.noreply.github.com>
2024-09-04 16:18:05 +0700

    update capstone

--- Project capstone/code_and_sql/automation.py
+++ Project capstone/code_and_sql/automation.py
@@ -3,15 +3,34 @@
 # Import libraries required for connecting to DB2 or PostgreSql
 
 # Connect to MySQL
+import mysql.connector
+connection = mysql.connector.connect(user='root', password='kFDvOxfbbLTzAdRsgHCpHFFl',host='172.21.73.185',database='sales')
+# create cursor
+cursor = connection.cursor()
 
-# Connect to DB2 or PostgreSql
+# Connect to PostgreSql
+import psycopg2
+dsn_hostname = '172.21.192.160'
+dsn_user='postgres'        # e.g. "abc12345"
+dsn_pwd ='gzQ4pZ4MaHaZFVlJJIJzusGN'      # e.g. "7dBZ3wWt9XN6$o0J"
+dsn_port ="5432"                # e.g. "50000" 
+dsn_database ="sales_database"           # i.e. "BLUDB"
+# create connection
+conn = psycopg2.connect(
+    database=dsn_database,
+    user=dsn_user,
+    password=dsn_pwd,
+    host=dsn_hostname,
+    port=dsn_port
+)
+cursor_pqs = conn.cursor()
 
-# Find out the last rowid from DB2 data warehouse or PostgreSql data warehouse
-# The function get_last_rowid must return the last rowid of the table sales_data on the IBM DB2 database or PostgreSql.
-
+# Function to get the last row_id
 def get_last_rowid():
-	pass
-
+    SQL = "SELECT MAX(ROWID) FROM sales_data"
+    cursor.execute(SQL)
+    row = cursor.fetchone()
+    return row
 
 last_row_id = get_last_rowid()
 print("Last row id on production datawarehouse = ", last_row_id)
@@ -20,23 +39,30 @@ print("Last row id on production datawarehouse = ", last_row_id)
 # The function get_latest_records must return a list of all records that have a rowid greater than the last_row_id in the sales_data table in the sales database on the MySQL staging data warehouse.
 
 def get_latest_records(rowid):
-	pass	
+    SQL = "SELECT * FROM sales_data WHERE rowid = (SELECT MAX(rowid) FROM sales_data);"
+    cursor.execute(SQL)
+    rowid = cursor.fetchone()
+    return rowid
+	
 
 new_records = get_latest_records(last_row_id)
 
-print("New rows on staging datawarehouse = ", len(new_records))
+print("New rows on staging datawarehouse = ", new_records)
 
 # Insert the additional records from MySQL into DB2 or PostgreSql data warehouse.
 # The function insert_records must insert all the records passed to it into the sales_data table in IBM DB2 database or PostgreSql.
 
 def insert_records(records):
-	pass
-
-insert_records(new_records)
-print("New rows inserted into production datawarehouse = ", len(new_records))
+     SQL = "INSERT INTO products (product_id, product_name, product_type, product_price) VALUES (%s, %s, %s, %s)"
+     cursor_pqs.execute(SQL, records)
+     conn.commit()
+     return cursor_pqs.rowcount
+    
+print("New rows inserted into production datawarehouse = ", new_records)
 
 # disconnect from mysql warehouse
 
+conn.close()
 # disconnect from DB2 or PostgreSql data warehouse 
-
+cursor_pqs.close()
 # End of program
